eee4ee90d09740a510e471dee4f479334ce7603c
Yakub Mohammad <yakub@arusatech.com>
2025-08-19 14:17:24 -0700

    ADAS_31:Published 0.1.5 version

--- xrayclient/__init__.py
+++ xrayclient/__init__.py
@@ -7,6 +7,6 @@ for test management in Jira.
 
 from .xray_client import JiraHandler, XrayGraphQL
 
-__version__ = "0.1.1"
+__version__ = "0.1.5"
 __author__ = "yakub@arusatech.com"
 __all__ = ["JiraHandler", "XrayGraphQL"] 
